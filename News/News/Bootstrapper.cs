4b3a77ee3b945d6a90d4e9d7a10934ad4371fe3e
Danny van Os <dannyvanos93@gmail.com>
2021-05-24 21:12:47 +0200

    Let's begin with the shell!

---
+++ News/News/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using News.ViewModels;
+
+namespace News
+{
+    public static class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterAssemblyTypes(typeof(App).Assembly)
+                .Where(x => x.IsSubclassOf(ViewModel));
+
+            var container = containerBuilder.Build();
+            Resolver.Initialize(container);
+        }
+    }
+}
