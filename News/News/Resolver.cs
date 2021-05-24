2068e07860bfdba95ac789074cfd560033bd2b2d
Danny van Os <dannyvanos93@gmail.com>
2021-05-24 20:45:32 +0200

    Add project files.

---
+++ News/News/Resolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using System.Text;
+
+namespace News
+{
+    public static class Resolver
+    {
+        private static IContainer container;
+
+        public static void Initialize(IContainer container)
+        {
+            Resolver.container = container;
+        }
+
+        public static T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
---
+++ News/News/ViewModels/HeadlineViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News.ViewModels
+{
+    public class HeadlineViewModel : ViewModel
+    {
+        public HeadlineViewModel()
+        {
+
+        }
+    }
+}
